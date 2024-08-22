fa13bc42bc35d93fb85be9e51d0772ef38e71c3f
devin <devinpixels25@gmail.com>
2024-08-22 12:24:39 -0400

    add getComputerChoice()

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-console.log('Hello World')
-console.log(5 + 5)
+// console.log('Hello World')
+// console.log(5 + 5)
+
+function getComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+    
+    if (randomNumber === 1) {
+        return 'rock';
+    } else if (randomNumber === 2) {
+        return 'paper';
+    } else if (randomNumber === 3) {
+        return 'scissors';
+    } else {
+        console.log('something went wrong')}
+}
+
+console.log(getComputerChoice())
